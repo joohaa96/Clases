ce3718ffadc9dda4fb2c421617c4a9e6351dbc7a
joohaa96 <johannacamacho69@outlook.es>
2024-03-05 19:05:02 +0100

    Second class Dani

---
+++ src/main/java/org/example/Clasedos.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Clasedos {
+
+
+    public static void main(String[] args) {
+        int number = 5;
+        int numberTwo = 3;
+
+        int numOne = 1;
+        int numTwo = 4;
+
+        multiplicar(number, numberTwo);
+        multiplicar(numOne, numTwo);
+
+        double numberDivideOne = 4.4;
+        double numberDivideTwo = 5.6;
+        double resultOne = dividir(numberDivideTwo, numberDivideOne);
+        System.out.println(resultOne);
+        
+    }
+
+
+    public static void multiplicar(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public static double dividir (double num1, double num2){
+        double resultado = num1 / num2;
+        return resultado;
+    }
+
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Main {
+
+    // public, private, protected
+    // static (opcional)
+    // boolean (true, false), byte (-128, 127), short, int, long, double, float, char, String
+    // void (en el caso de que quieras que el metodo no devuelva nada)
+
+    public static int num = 6;
+    public static int num1 = 2;
+
+
+    public static void main(String[] args) {
+        sumTwoNumbers(num, num1);
+        int resta = restarDosNumeros(num, num1);
+        System.out.println(resta);
+    }
+
+    public static void sumTwoNumbers(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+
+    public static int restarDosNumeros(int num1, int num2){
+        int result = num1 - num2;
+        return result;
+    }
+
+    // 2 metodos:
+    // 1. Quiero que me creas un metodo que multiplica 2 numeros y los imprime por pantalla pero no devuelve nada
+    // 2. Quiero que me creas un metodo que divida 2 numeros y devuelva el resultado y el resultado lo guardes en una varibale
+    // Descargar git utilizando el comando git init
+
+//ejercico 1
+
+    //camelCase
+
+
+}
