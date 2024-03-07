0f592a3628a1f47631c4b3bd5f03cc73fd58318d
joohaa96 <johannacamacho69@outlook.es>
2024-03-07 19:32:39 +0100

    Suma dos numeros

---
+++ src/main/java/org/example/Deberes.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Deberes {
+    //Crear tu rama de deberes
+
+    //1. Un metodo que coge como argumentos 2 numeros y imprmir el numero mas grande
+    //   Hacer tambien la comprobacion en el caso de que los numeros son iguales y si son iguales imprimir "Son iguales"
+
+    //2. Un metodo que compara 2 nombres independientemente de sus el nombre contiene mayusculas o no.
+    //   Si los nombres son iguales imprimir: "Los nombres son iguales"
+    //   Si los nombres no son iguales imprimir: "Los nombres no son iguales"
+
+    //Despues de completar los deberes mergear tu rama a develop
+
+    public static void main(String[] args) {
+        //Aqui va la declaracion de las variables y las llamadas al metodo
+    }
+
+    //Aqui va el metodo ---->
+
+}
