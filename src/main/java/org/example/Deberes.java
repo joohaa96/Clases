41484b1404bbf6763c4484c5d638cf29551d511b
joohaa96 <johannacamacho69@outlook.es>
2024-03-13 20:22:48 +0100

    Ejercicio Array

--- src/main/java/org/example/Deberes.java
+++ src/main/java/org/example/Deberes.java
@@ -8,7 +8,7 @@ public class Deberes {
     //1. Un metodo que coge como argumentos 2 numeros y imprmir el numero mas grande
     //   Hacer tambien la comprobacion en el caso de que los numeros son iguales y si son iguales imprimir "Son iguales"
 
-    //2. Un metodo que compara 2 nombres independientemente de sus el nombre contiene mayusculas o no.
+    //2. Un metodo que compara 2 nombres independientemente de si el nombre contiene mayusculas o no.
     //   Si los nombres son iguales imprimir: "Los nombres son iguales"
     //   Si los nombres no son iguales imprimir: "Los nombres no son iguales"
 
@@ -24,40 +24,44 @@ public class Deberes {
         int b= 3;
 
         String name= "Yan";
+        String nameTwo = "yan";
         String nameOne= "Marco";
 
-        names(name,nameOne);
+        compraNames(name,nameTwo);
 
-
-        variable(a,b);
-
-        /*int max= Math.max(num, numOne);
-        System.out.println(max); */ //podria valer como metodo de comparacion
-
-        if (num < numOne) {
-            System.out.println( num + " es menor que" + " " + numOne );
-    }
-        else {
-        System.out.println( numOne + " es mayor" + " " + num );
-    }
-
+        maxNum(a,b);
+        maxNum(num,numOne);
 
     }
 
         //Aqui va el metodo ---->
-        public static void variable (int a, int b){
-        if (a==b) {
-        System.out.println("Son iguales");
+        public static void maxNum (int a, int b){
+            if (a==b) {
+                System.out.println("Son iguales");
+            }
+            if (a < b) {
+                System.out.println( a + "es menor que"+ " " + b);
+            }
+            else {
+                System.out.println( b + "es mayor que" + " " + a);
+            }
+        }
+
+        public static void maxNumByDani(int a, int b) {
+            if (a == b){
+                System.out.println("Son iguales");
+            }
+            else if (a < b) {
+                System.out.println( a + "es menor que"+ " " + b);
+            }
+            else {
+                System.out.println( b + "es mayor que" + " " + a);
+            }
+        }
 
 
-
-    }
-
-
-
-    }
-    public static void names (String name, String nameOne){
-        if (name.equals( nameOne)) {
+    public static void compraNames (String name, String nameOne){
+        if (name.equalsIgnoreCase(nameOne)) {
             System.out.println( "Los nombres son iguales");
         }
             else {
---
+++ src/main/java/org/example/EjerciciosArray.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class EjerciciosArray {
+    public static void main (String[] args){
+        int[] arrayNum= {1,2,3,4,5,6,7,8,9,10};
+        int mayor= 0;
+
+
+        String[] nombreDividido = {"j","o", "h", "a", "n", "n", "a"};
+
+        printNum(arrayNum, mayor);
+        printNombres(nombreDividido);
+    }
+    public static void printNum (int[] arrayNum, int mayor){
+        for (int i = 0; i < arrayNum.length; i++) {
+            if (arrayNum[i]> i) {
+                mayor=arrayNum[i];
+            }
+        }
+            System.out.println("El numero mayor es " + mayor );
+    }
+    public static  void printNombres (String[]nombreDividio){
+        for (int i = nombreDividio.length - 1; i >= 0; i--) {
+            System.out.println(nombreDividio[i]);
+
+        }
+
+    }
+
+}
+
+
+
+
